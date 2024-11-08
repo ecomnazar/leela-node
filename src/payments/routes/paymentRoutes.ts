f83acfdc287637ea26f83f9b54e2449b209e2b69
ecomnazar <ecomnazar.me@gmail.com>
2024-11-08 13:07:24 +0500

    [fixed]: again webhook

--- src/payments/routes/paymentRoutes.ts
+++ src/payments/routes/paymentRoutes.ts
@@ -9,10 +9,11 @@ router.post("/notification/rukassa", handlePayment);
 
 router.post("/webhook-h", (req, res) => {
   // Передаем обновление от Telegram в bot.handleUpdate
-  console.log(JSON.stringify(req.body));
-  // bot
-  //   .handleUpdate(req.body)
-  //   .then(() => res.sendStatus(200)) // Успешно обработано — статус 200
-  //   .catch(() => res.sendStatus(500)); // Ошибка обработки — статус 500
+  console.log("before");
+  bot
+    .handleUpdate(req.body)
+    .then(() => res.sendStatus(200)) // Успешно обработано — статус 200
+    .catch(() => res.sendStatus(500)); // Ошибка обработки — статус 500
+  console.log("after");
   res.send("ok");
 });
