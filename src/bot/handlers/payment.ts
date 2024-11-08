fde0e5f87e2b759c76583126513b565028aecf1a
ecomnazar <ecomnazar.me@gmail.com>
2024-11-08 14:18:27 +0500

    [fixed]: payment

--- src/bot/handlers/payment.ts
+++ src/bot/handlers/payment.ts
@@ -30,7 +30,7 @@ export const createPayment = async (userId: number, amount: number) => {
   ];
 
   const response = await axios.post(
-    `${BASE_URL}?shop_id=${SHOP_ID}&order_id=${orderId}&amount=${amount}&token=${TOKEN}&user_code=${orderId}&${cards
+    `${BASE_URL}?shop_id=${SHOP_ID}&order_id=${orderId}&amount=${amount}&token=${TOKEN}&method=sbp&user_code=${orderId}&${cards
       .map((n, index) => `list[${index}]=${n}`)
       .join("&")}`
   );
