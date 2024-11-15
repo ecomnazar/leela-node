255c7324dfb9157319621e55a3415c9f98d67530
ecomnazar <ecomnazar.me@gmail.com>
2024-11-15 16:06:43 +0500

    [test]: star payments

--- src/bot/lib/createInvoiceLink.ts
+++ src/bot/lib/createInvoiceLink.ts
@@ -5,7 +5,7 @@ require("dotenv").config();
 export const createInvoiceLink = async (chatId: number, usdAmount: number) => {
   const token = process.env.BOT_TOKEN;
   const payload = Date.now();
-  const amount = usdAmount * 50;
+  const amount = usdAmount;
   const prices = JSON.stringify([{ label: "Plan", amount }]);
   const url = `https://api.telegram.org/bot${token}/createInvoiceLink?chat_id=${chatId}&title=Test&description=testDesc&payload=${payload}&currency=XTR&prices=${prices}`;
 
